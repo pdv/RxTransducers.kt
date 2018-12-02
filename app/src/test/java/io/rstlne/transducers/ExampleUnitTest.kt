21984e120e0a9383bffe2282eea1f2153d1266a3
pdv <philipjdelvecchio@gmail.com>
2018-12-01 20:20:00 -0500

    initial commit

---
+++ app/src/test/java/io/rstlne/transducers/ExampleUnitTest.kt
@@ -0,0 +1,40 @@
+package io.rstlne.transducers
+
+import io.reactivex.Observable
+import net.onedaybeard.transducers.filter
+import net.onedaybeard.transducers.map
+import net.onedaybeard.transducers.plus
+import org.junit.Test
+import kotlin.math.sqrt
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+
+    @Test
+    fun addition_isCorrect() {
+
+        val rxStart = System.currentTimeMillis()
+        Observable.range(0, 10000000)
+            .filter { it % 2 != 0 }
+            .map { sqrt(it.toFloat()) }
+            .blockingLast()
+        val rxEnd = System.currentTimeMillis()
+        println("Rx chain: ${rxEnd - rxStart}ms")
+
+        val xfStart = System.currentTimeMillis()
+        Observable.range(0, 10000000)
+            .transduce(
+                filter<Int> { it % 2 != 0 } +
+                map<Float, Int> { sqrt(it.toFloat()) }
+            )
+            .blockingLast()
+        val xfEnd = System.currentTimeMillis()
+        println("xf chain: ${xfEnd - xfStart}ms")
+
+    }
+
+}
